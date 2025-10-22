929978f153bbe8f25dcec75b427bef33bcaf31fe
jpgianfaldoni <jpgianfaldoni@gmail.com>
2025-10-22 10:55:31 -0300

    remove unecessary sg rule

--- workspace-setup/terraform-examples/aws/aws-byovpc/tf/security_group.tf
+++ workspace-setup/terraform-examples/aws/aws-byovpc/tf/security_group.tf
@@ -10,16 +10,4 @@ resource "aws_security_group_rule" "default_sg_egress_ports" {
   depends_on        = [module.vpc]
 }
 
-# Add egress rule for SQL Server connectivity (port 1433)
-resource "aws_security_group_rule" "default_sg_egress_sql_server" {
-  count             = var.vpc_id == "" ? 1 : 0
-  type              = "egress"
-  from_port         = 1433
-  to_port           = 1433
-  protocol          = "tcp"
-  cidr_blocks       = [var.vpc_cidr_range]
-  security_group_id = module.vpc[0].default_security_group_id
-  description       = "Allow outbound TCP traffic to SQL Server on port 1433"
-  depends_on        = [module.vpc]
-}
 

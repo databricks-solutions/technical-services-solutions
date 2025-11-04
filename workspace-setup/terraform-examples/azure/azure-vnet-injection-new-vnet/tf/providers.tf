7ee0acb52c8369b910cd73dff3f1e913f317fc2e
Gergelj Kis <gergelj.kis@databricks.com>
2025-11-04 19:55:03 +0100

    added versions file

--- workspace-setup/terraform-examples/azure/azure-vnet-injection-new-vnet/tf/providers.tf
+++ workspace-setup/terraform-examples/azure/azure-vnet-injection-new-vnet/tf/providers.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-    }
-  }
-}
-
 provider "azurerm" {
   subscription_id = var.azure_subscription_id
   features {}

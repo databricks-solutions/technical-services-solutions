55b46038e590a855f5e0770b7fbde3cf588c4825
Gergelj Kis <gergelj.kis@databricks.com>
2025-11-04 21:01:47 +0100

    variable validation added

--- workspace-setup/terraform-examples/azure/azure-vnet-injection-new-vnet/tf/variables.tf
+++ workspace-setup/terraform-examples/azure/azure-vnet-injection-new-vnet/tf/variables.tf
@@ -28,13 +28,27 @@ variable "workspace_name" {
 }
 
 variable "root_storage_name" {
-    description = "The name of the root storage account"
-    type        = string
+  type        = string
+  description = "The root storage name. Only lowercase letters and numbers, 3-24 characters."
+  validation {
+    condition     = length(var.root_storage_name) >= 3 && length(var.root_storage_name) <= 24
+    error_message = "root_storage_name must be between 3 and 24 characters."
+  }
+  validation {
+    condition     = can(regex("^[a-z0-9]+$", var.root_storage_name))
+    error_message = "root_storage_name can only contain lowercase letters and numbers."
+  }
 }
 
 variable "location" {
     description = "The Azure region to deploy the workspace to"
     type        = string
+    validation {
+    condition = contains([
+      "australiacentral", "australiacentral2", "australiaeast", "australiasoutheast", "brazilsouth", "canadacentral", "canadaeast", "centralindia", "centralus", "chinaeast2", "chinaeast3", "chinanorth2", "chinanorth3", "eastasia", "eastus", "eastus2", "francecentral", "germanywestcentral", "japaneast", "japanwest", "koreacentral", "mexicocentral", "northcentralus", "northeurope", "norwayeast", "qatarcentral", "southafricanorth", "southcentralus", "southeastasia", "southindia", "swedencentral", "switzerlandnorth", "switzerlandwest", "uaenorth", "uksouth", "ukwest", "westcentralus", "westeurope", "westindia", "westus", "westus2", "westus3"
+    ], var.location)
+    error_message = "Valid values for var.location are standard Azure regions supported by Databricks."
+  }
 }
 
 # =============================================================================

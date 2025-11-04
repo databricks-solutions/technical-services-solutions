424bb1e6278244d723f89c986b73d0ac768a49d2
Gergelj Kis <gergelj.kis@databricks.com>
2025-11-04 19:20:01 +0100

    cmk parameters removed

--- workspace-setup/terraform-examples/azure/azure-vnet-injection-new-vnet/tf/databricks.tf
+++ workspace-setup/terraform-examples/azure/azure-vnet-injection-new-vnet/tf/databricks.tf
@@ -5,13 +5,6 @@ resource "azurerm_databricks_workspace" "this" {
   sku                 = "premium"
   tags                = var.tags
 
-#   Customer managed key (CMK) configuration
-#   customer_managed_key_enabled          = true
-#   managed_services_cmk_key_vault_id     = var.managed_services_cmk_key_vault_id
-#   managed_services_cmk_key_vault_key_id = var.managed_services_cmk_key_vault_key_id
-#   managed_disk_cmk_key_vault_id         = var.managed_disk_cmk_key_vault_id
-#   managed_disk_cmk_key_vault_key_id     = var.managed_disk_cmk_key_vault_key_id
-
   custom_parameters {
     virtual_network_id                                   = azurerm_virtual_network.this.id
     private_subnet_name                                  = azurerm_subnet.private.name

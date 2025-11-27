94b9575795a9400906ebc5bb53142676093043c5
Gergelj Kis <gergelj.kis@databricks.com>
2025-11-27 20:36:15 +0100

    remove existing vnet directory

--- workspace-setup/terraform-examples/azure/azure-vnet-injection-existing-vnet/tf/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "databricks_workspace_id" {
-  value = azurerm_databricks_workspace.this.id
-}
-
-output "workspace_url" {
-  value = "https://${azurerm_databricks_workspace.this.workspace_url}/"
-}

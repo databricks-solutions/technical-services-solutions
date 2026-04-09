4e701849ebe4edee61be6fbe52e7c1bf3b75ef20
joseDelgado9898 <josedv9898@gmail.com>
2026-04-09 16:09:57 -0600

    Implementation of aws-byovpc-uc , adds a default external location and catalog when deploying AWS resources

---
+++ workspace-setup/terraform-examples/aws/aws-byovpc-uc/tf/versions.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = "~> 1.3"
+
+  required_providers {
+    databricks = {
+      source  = "databricks/databricks"
+      version = "~> 1.84"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.76, < 7.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.9"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
+}
+

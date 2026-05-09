da73f70a53a3c576f5a6690763d3a79e2e027616
Eri Adepoju <eri.adepoju@databricks.com>
2026-05-09 01:40:59 -0500

    Update Migration Accelerator app files and configuration

--- data-warehousing/lakebridge-analyzer-lineage/src/migration_accelerator/apis/discovery.py
+++
@@ -1,5 +0,0 @@
-from migration_accelerator.discovery.lineage import get_lineage
-
-
-def lineage_api() -> str:
-    return get_lineage()
--- data-warehousing/lakebridge-analyzer-lineage/src/migration_accelerator/configs/__init__.py
+++
@@ -1,3 +0,0 @@
-from migration_accelerator.configs.main import get_config, set_config
-
-__all__ = ["get_config", "set_config"]
